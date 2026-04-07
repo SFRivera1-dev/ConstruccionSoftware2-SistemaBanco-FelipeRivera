f226eedcb5aa34797c40c2eb1eb7dd9c54a71077
SFRivera1-dev <felipe.rivera@correo.tdea.edu.co>
2026-04-07 12:07:49 -0500

    CHG: Implement CreditPort methods

--- SistemaBanco/src/main/java/app/domain/ports/CreditPort.java
+++ SistemaBanco/src/main/java/app/domain/ports/CreditPort.java
@@ -1,5 +1,13 @@
 package app.domain.ports;
 
-public class CreditPort {
+import java.util.List;
+import app.domain.models.Credit;
+import app.domain.models.CreditStatus;
+
+public interface CreditPort {
+    void save(Credit credit);
+    Credit findById(Long id);
+    List<Credit> findByCustomerDocument(Long document);
+    void updateStatus(Long creditId, CreditStatus newStatus);
 
 }
