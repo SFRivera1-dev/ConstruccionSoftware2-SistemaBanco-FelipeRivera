591c042ff41b9ef5a6db36241f8e7b29a2216563
SFRivera1-dev <felipe.rivera@correo.tdea.edu.co>
2026-04-07 12:08:07 -0500

    CHG: Implement TransferPort methods

--- SistemaBanco/src/main/java/app/domain/ports/TransferPort.java
+++ SistemaBanco/src/main/java/app/domain/ports/TransferPort.java
@@ -1,5 +1,13 @@
 package app.domain.ports;
 
-public class TransferPort {
+import app.domain.models.Transfer;
+import app.domain.models.TransferStatus;
+import java.util.List;
 
+public interface TransferPort {
+    void save(Transfer transfer);
+    Transfer findById(Long id);
+    List<Transfer> findByAccountNumber(String accountNumber);
+    List<Transfer> findByStatus(TransferStatus status);
+    void updateStatus(Long transferId, TransferStatus newStatus);
 }
