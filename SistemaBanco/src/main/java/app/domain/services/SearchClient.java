4c946e4eee34b3773885e269ed853202bd26447c
SFRivera1-dev <felipe.rivera@correo.tdea.edu.co>
2026-04-07 12:27:25 -0500

    FEAT: implement SearchClient service logic

--- SistemaBanco/src/main/java/app/domain/services/SearchClient.java
+++ SistemaBanco/src/main/java/app/domain/services/SearchClient.java
@@ -1,5 +1,34 @@
 package app.domain.services;
 
+import app.domain.Exceptions.BusinessException;
+import app.domain.models.Customer;
+import app.domain.ports.ClientPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SearchClient {
 
+    private final ClientPort clientPort;
+
+    @Autowired
+    public SearchClient(ClientPort clientPort) {
+        this.clientPort = clientPort;
+    }
+
+    public Customer findByDocument(Long document) throws BusinessException {
+        Customer customer = clientPort.findByDocument(document);
+        if (customer == null) {
+            throw new BusinessException("No existe un cliente con ese documento");
+        }
+        return customer;
+    }
+
+    public Customer findById(Long id) throws BusinessException {
+        Customer customer = clientPort.findById(id);
+        if (customer == null) {
+            throw new BusinessException("No existe un cliente con ese ID");
+        }
+        return customer;
+    }
 }
