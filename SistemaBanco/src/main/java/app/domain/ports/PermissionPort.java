0bbddeb76e201642ec26182d972cd29a003e9965
SFRivera1-dev <felipe.rivera@correo.tdea.edu.co>
2026-04-07 12:07:58 -0500

    CHG: Implement PermissionPort methods

--- SistemaBanco/src/main/java/app/domain/ports/PermissionPort.java
+++ SistemaBanco/src/main/java/app/domain/ports/PermissionPort.java
@@ -1,5 +1,12 @@
 package app.domain.ports;
 
-public class PermissionPort {
+import java.util.List;
+
+import app.domain.models.User;
+
+public interface PermissionPort {
+    void grantPermission(Long companyDocument, Long targetUserId);
+    void revokePermission(Long companyDocument, Long targetUserId);
+    List<User> findUsersByCompany(Long companyDocument);
 
 }
