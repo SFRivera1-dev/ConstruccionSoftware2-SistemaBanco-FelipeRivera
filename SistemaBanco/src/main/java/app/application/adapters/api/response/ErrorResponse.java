dd761b3b71b586be907da000d23249bb1df1c69d
SFRivera1-dev <felipe.rivera@correo.tdea.edu.co>
2026-04-20 18:42:57 -0500

    Fix: Fixes for the “response” error and to ensure compatibility with the globalExceptionhandler

--- SistemaBanco/src/main/java/app/application/adapters/api/response/ErrorResponse.java
+++ SistemaBanco/src/main/java/app/application/adapters/api/response/ErrorResponse.java
@@ -4,10 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class ErrorResponse {
-    private String message;
     private int status;
+    private String message;
+    private List<String> errors;
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+        this.errors = null;
+    }
 }
