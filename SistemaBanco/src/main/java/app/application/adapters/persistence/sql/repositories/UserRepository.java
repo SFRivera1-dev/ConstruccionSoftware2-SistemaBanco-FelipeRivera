3579f5804ccef1ec62f3aa85ab95390aebd68058
SFRivera1-dev <felipe.rivera@correo.tdea.edu.co>
2026-04-22 23:19:14 -0500

    feat: add query methods to UserRepository

--- SistemaBanco/src/main/java/app/application/adapters/persistence/sql/repositories/UserRepository.java
+++ SistemaBanco/src/main/java/app/application/adapters/persistence/sql/repositories/UserRepository.java
@@ -4,8 +4,14 @@ import app.application.adapters.persistence.sql.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByDocument(String document);
+    UserEntity findByUsername(String username);
     UserEntity findByEmail(String email);
+    boolean existsByDocument(String document);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
