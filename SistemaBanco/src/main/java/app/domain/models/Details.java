cf1d47f000181d4a2173e8bbf5061d2efad477e3
SFRivera1-dev <felipe.rivera@correo.tdea.edu.co>
2026-05-02 00:53:43 -0500

    feat: add fields to Details class for binnacle data

--- SistemaBanco/src/main/java/app/domain/models/Details.java
+++ SistemaBanco/src/main/java/app/domain/models/Details.java
@@ -1,5 +1,32 @@
 package app.domain.models;
 
-public class Details {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Details {
+    // Para transferencias
+    private BigDecimal mount;
+    private BigDecimal balanceBeforeOrigin;
+    private BigDecimal balanceAfterOrigin;
+    private BigDecimal balanceBeforeDestination;
+    private BigDecimal balanceAfterDestination;
+
+    // Para créditos
+    private BigDecimal amountApproved;
+    private BigDecimal interestRate;
+    private String previousStatus;
+    private String newStatus;
+
+    // Para vencimiento de transferencia
+    private String reason;
+
+    // Para cuentas
+    private String accountNumber;
+    private String accountType;
 }
